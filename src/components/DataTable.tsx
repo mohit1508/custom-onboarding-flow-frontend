db66656483d63db9baff6d4d7f8bc0b6e0bb2712
mohit1508 <mohitkhandelwal1997@yahoo.com>
2025-01-21 20:37:44 -0500

    correcting date in data table

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -29,7 +29,7 @@ const DataTable: React.FC = () => {
     const date = new Date(dateString); 
 
     const month = String(date.getMonth() + 1).padStart(2, "0"); 
-    const day = String(date.getDate()).padStart(2, "0");
+    const day = String(date.getDate() + 1).padStart(2, "0");
     const year = date.getFullYear();
 
     return `${month}-${day}-${year}`; // Format as MM-DD-YYYY
